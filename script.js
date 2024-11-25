9a4bc79c8436f00d3629a492aa15161537372daa
Gianfranco <120363150+cuzzi06@users.noreply.github.com>
2024-11-24 20:05:32 -0500

    Agregar función para capturar la elección del usuario

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function getComputerChoice() {
-    const choice = Math.floor(Math.random() * 3)
+    const choice = Math.floor(Math.random() * 3);
 
     if (choice == 0) {
         return('Rock')
@@ -11,3 +11,16 @@ function getComputerChoice() {
 
 }
 
+function getHumanChoice () {
+    const humanChoice = prompt('Rock, paper, scissors?', ''); 
+
+    if (humanChoice == 'Rock' || humanChoice == 'rock') {
+        return(1)
+    } else if (humanChoice == 'Paper' || humanChoice == 'paper') {
+        return(2)
+    } else if (humanChoice == 'Scissors' || humanChoice == 'scissors') {
+        return(3)
+    } else {
+        alert('Opción inválida')
+    }
+}
