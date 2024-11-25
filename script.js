298e2d6c7d91b0c23b07c235356d633e4021da69
Gianfranco <120363150+cuzzi06@users.noreply.github.com>
2024-11-24 20:20:52 -0500

    Agregar inicialización de puntuaciones y convertir elección del usuario a minúsculas

--- script.js
+++ script.js
@@ -12,15 +12,18 @@ function getComputerChoice() {
 }
 
 function getHumanChoice () {
-    const humanChoice = prompt('Rock, paper, scissors?', ''); 
+    const humanChoice = prompt('Rock, paper, scissors?', '').toLowerCase(); 
 
-    if (humanChoice == 'Rock' || humanChoice == 'rock') {
+    if (humanChoice == 'rock') {
         return(1)
-    } else if (humanChoice == 'Paper' || humanChoice == 'paper') {
+    } else if (humanChoice == 'paper') {
         return(2)
-    } else if (humanChoice == 'Scissors' || humanChoice == 'scissors') {
+    } else if (humanChoice == 'scissors') {
         return(3)
     } else {
         alert('Opción inválida')
     }
 }
+
+let humanScore = 0;
+let computerScore = 0; 
